c1f769c08c69e6d7715344776ea7c53c92c045ad
Redvan <redvanisation@gmail.com>
2019-08-13 17:48:21 -0400

    creating the static pages home help and about

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "shoud get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
 end
