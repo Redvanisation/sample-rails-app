7cfe4cabff7bbc789343770cd697c01380f45ffd
Redvan <redvanisation@gmail.com>
2019-08-20 18:52:23 -0400

    Creating partials and updating the pages paths/tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  get  '/about',   to: 'static_pages#about'
+  get  '/help',    to: 'static_pages#help'
+  get  '/contact', to: 'static_pages#contact'
   
 
 
