146af85945570c074f3843df5fa2c9d37a75ae85
Redvan <redvanisation@gmail.com>
2019-08-20 19:12:55 -0400

    Setting up and testing the signup page template

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/help',    to: 'static_pages#help'
   get  '/contact', to: 'static_pages#contact'
+  get '/signup',   to: 'users#new'
   
 
 
