4d25d191f5bc11b521bb5a8ad204d74fde0ad2b7
Redvan <redvanisation@gmail.com>
2019-09-04 05:19:10 -0400

    finishing up the signup functionality and tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
    
     assert_template 'users/new'
   end
+
+  test "valid signup infos" do
+    get signup_path
+
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                                      email: "user@example.com",
+                                                      password:              "password",
+                                                      password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
 end
