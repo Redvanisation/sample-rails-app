4f287d7a80c3ca98272d7dacfd45ca1338c9d5a8
Redvan <redvanisation@gmail.com>
2019-09-14 18:51:08 -0400

    making the login work and testing it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      #Log the user in and redirect to the user's show page
+      log_in user
+      redirect_to user
+    else
+      #Show an error message
+      flash.now[:danger] = "Invalid email/password combiantion"
+      render 'new'
+    end
+  end
+
+  def destroy
+
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+
+    #logs in the given user
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    # Returns the current logged in user (if any)
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    # Checks if the user is logged in
+    def logged_in?
+        !current_user.nil?
+    end
 end
