77156d63a59724251b24dec7a16b6708e81d6318
Redvan <redvanisation@gmail.com>
2019-09-14 21:26:01 -0400

    adding the remember user functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #Log the user in and redirect to the user's show page
       log_in user
+      remember user
       redirect_to user
     else
       #Show an error message
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
