e1908abda8ae457e02806ca2abd32874249e3015
Redvan <redvanisation@gmail.com>
2019-09-15 20:25:01 -0400

    Add users index to list all the registered users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
 end
