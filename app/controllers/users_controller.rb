744760315ca1978cf280c95a1254529e65445ec7
Redvan <redvanisation@gmail.com>
2019-09-15 21:33:12 -0400

    Add pagination and tests to users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def new
