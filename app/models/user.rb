0a84d80871ae9a52cbc6e56e692fedde3afe7bfb
Redvan <redvanisation@gmail.com>
2019-09-20 20:07:01 -0400

    Add reset password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 
@@ -49,9 +49,25 @@ class User < ApplicationRecord
     end
 
     # Sends the activation email
-
     def send_activation_email
       UserMailer.account_activation(self).deliver_now
+    end
+
+    # Sets the password rest attributes
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_attribute(:reset_digest, User.digest(reset_token))
+      update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # Sends the password reset email
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
+
+    # Returns true if password reset has expired
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'account_activations/edit'
 
   root 'static_pages#home'
@@ -15,7 +19,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
-
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
